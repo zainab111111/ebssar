8a795e0a919945dcae47a2494aedabea6be31c1e
Al-aqua <mohammad@al-aqua.dev>
2025-08-27 21:56:54 +0300

    Fix bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'testing') {
+        if (config('app.env') === 'local') {
             URL::forceScheme('https');
         }
     }
