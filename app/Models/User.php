8acd26031efe97526ff68311f91a760520b102c2
Al-aqua <mohammad@al-aqua.dev>
2025-09-09 22:03:21 +0300

    Fix bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,6 +66,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@ebssar.com') && $this->hasVerifiedEmail();
+        /* return str_ends_with($this->email, '@ebssar.com') && $this->hasVerifiedEmail(); */
+        return str_ends_with($this->email, '@ebssar.com');
     }
 }
